ef4dcb2d95380f4e4cd79c4b24fb409ec9d688e9
Elijah-Vakulenko <154387383+Elijah-Vakulenko@users.noreply.github.com>
2024-07-17 22:02:20 +0100

    grid generator+

--- src/components/Content/Css.jsx
+++ src/components/Content/Css.jsx
@@ -36,6 +36,9 @@ const Css = () => {
             <li>
                 <a className="link-with-favicon" href="https://www.linkedin.com/posts/valeriia-hrytsyshyna_%D0%B9%D0%BE%D1%83-%D0%B4%D1%80%D1%83%D0%B7%D1%96-%D1%8F-%D0%B4%D0%BE-%D0%B2%D0%B0%D1%81-%D0%B7-%D0%BA%D1%80%D1%83%D1%82%D0%B8%D0%BC-%D0%BC%D0%B0%D1%82%D0%B5%D1%80%D1%96%D0%B0%D0%BB%D0%BE%D0%BC-activity-7207361401437302784-NQcy?utm_source=share&utm_medium=member_android">nth-child selector quick guide</a>
             </li>
+            <li>
+                <a className="link-with-favicon" href="https://css-grid-generator.netlify.app/">Grid Generator</a>
+            </li>
         </ul>
     )
 };
