042031700566201b8bb5e816844d11755c3c4ec3
Elijah-Vakulenko <154387383+Elijah-Vakulenko@users.noreply.github.com>
2024-07-24 11:16:34 +0100

    links+

--- src/components/Content/Upd.jsx
+++ src/components/Content/Upd.jsx
@@ -3,13 +3,26 @@ import React from 'react'
 const upd = () => {
   return (
       <div>
-          <mark>July 2024</mark>
-      <ul className='updList'>
-        <li>23.07.2024 - Milord Animation Greetings added</li>
-            <li>22.07.2024 - New folder created <kbd>Desktop Updates</kbd></li>
-            <li>22.07.2024 - New folder created <kbd>Cheatsheet Gallery</kbd></li>
-            <li>21.07.2024 - <kbd>Js tasks</kbd> added to JavaScript</li>
-          </ul>
+        
+      <fieldset>
+        <legend>July 2024</legend>
+          <ul className='updList'>
+            <li>24.07.2024 - Added links for new React lesson youtube + github <kbd>Модуль 5. Заняття 1. React Router</kbd></li>
+            <li>23.07.2024 - Milord Animation Greetings added</li>
+                <li>22.07.2024 - New folder created <kbd>Desktop Updates</kbd></li>
+                <li>22.07.2024 - New folder created <kbd>Cheatsheet Gallery</kbd></li>
+          <li>21.07.2024 - <kbd>Js tasks</kbd> added to JavaScript</li>
+          <li>14.07.2024 - Web Dev Desktop v2.0 launched</li>
+              </ul>
+      </fieldset>
+     
+      <fieldset>
+        <legend>June 2024</legend>
+        <ul>
+          <li>28.06.2024 - Project launched</li>
+        </ul>
+      </fieldset>
+
     </div>
   )
 }
